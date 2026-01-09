41590260e50aa3b3fdce12c6a06f9fee511b4b1e
Danny Ben Shitrit <db@dannyben.com>
2026-01-09 16:23:25 +0000

    fix specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,6 +21,5 @@ ENV['LINES'] = '30'
 
 RSpec.configure do |config|
   config.example_status_persistence_file_path = 'spec/status.txt'
-  config.strip_ansi_escape = true
   config.filter_run_excluding :noci if ENV['CI']
 end
